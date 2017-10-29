36047dab3252843763b25414439f2ac18e523ef5
ramonacaprariu <ramonacaprariu@gmail.com>
2017-10-29 19:34:14 -0400

    Add files via upload

---
+++ sketch-6.js
@@ -0,0 +1,23 @@
+
+
+
+
+function setup()
+{
+createCanvas(windowWidth, windowHeight);
+  background(204,255,204);
+}
+
+
+function draw()
+{
+fill(255,204,229);
+    noStroke(0);
+ellipse(windowWidth/2, windowHeight/2, 400, 400)
+
+
+}
+
+function touchMoved() {
+  return false;
+}
