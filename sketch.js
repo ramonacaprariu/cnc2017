a23f532067b426e21d2a783d1dd796e864d13081
ramonacaprariu <ramonacaprariu@gmail.com>
2017-10-29 16:11:24 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,60 +1,33 @@
-/*
- * Creation & Computation - Digital Futures, OCAD University
- * Kate Hartman / Nick Puckett
- * 
- * Fade between 2 colors
- * adapted from the LED fading arduino example
- * 
- */
+// ICM-2016
 
-         //Pin that the led is attached to - check that it is a PWM pin  
-var fadeRate = 10;        //the time between updates in milliseconds
-var fadeIncrement = 0.005;    // how much to change the brightness each cycle 
+// Take a look at the HTML file where some things have been
+// added for mobile viewing
 
-
-var color1;
-var color2;
-var xFadeVal = 0;
-
-var lastTimeYouChanged = 0;  //this stores the time of the last change
-
+var colors;
 
 function setup() {
-               
-
+  // Make the canvas the size of the mobile device screen
   createCanvas(windowWidth, windowHeight);
-  color1 = color(255, 204, 229);
-  color2 = color(204, 255, 204);
+  background(200);
+
+  // An array of five colors, one for each finger
+  colors = [color(255,0,0), color(0,255,0), color(0,0,255), color(255, 255,0), color(0,255,255)];
 }
 
-function draw() 
-{
-
-background(204,229,255);
-
-fill(lerpColor(color1,color2,xFadeVal));
-noStroke();
-ellipse(width/2,height/2,height/3,height/3);
-
-if(millis()-lastTimeYouChanged>=fadeRate) //this very simple statement is the timer,
-{                                          //it subtracts the value of the moment in time the last blink happened, and sees if that number is larger than your set blinking value
-
-xFadeVal += fadeIncrement;
-
-  if (xFadeVal <= 0 || xFadeVal >= 1) {
-    fadeIncrement = -fadeIncrement;
+function draw() {
+  // The touches array holds an object for each and every touch
+  // The array length is dynamic and tied to the number of fingers 
+  // currently touching
+  for (var i = 0; i < touches.length; i++) {
+    noStroke();
+    // One color per finger
+    fill(colors[i]);
+    // Draw a circle at each finger
+    ellipse(touches[i].x, touches[i].y, 24, 24);
   }
-
-lastTimeYouChanged = millis();            //save the value in time that this switch occured, so we can use it again.
-   
 }
 
-
-
-}
-
-
-function windowResized() 
-{
-  resizeCanvas(windowWidth, windowHeight);
+// this prevents dragging screen around
+function touchMoved() {
+  return false;
 }
